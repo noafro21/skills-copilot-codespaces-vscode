929fb24841ccfb024b36335c2ad267fe6dc6b189
noafro21 <137956923+noafro21@users.noreply.github.com>
2025-07-10 23:18:09 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,15 @@
+function skillsMember() {
+    return [
+        'JavaScript',
+        'HTML',
+        'CSS',
+        'React',
+        'Node.js',
+        'Express',
+        'MongoDB',
+        'SQL',
+        'Python',
+        'Django'
+    ];       
+    
+}
